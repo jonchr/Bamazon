f1fb59507e3c941b2224fc0a55089f45f9f505b1
Jon <jonathan.lee.christensen@gmail.com>
2017-08-28 16:21:47 -0400

    added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,10 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+//SETUP
+// ===========================================================
 
+var mysql = require("mysql"); //node MySQL module
+var inquirer = require("inquirer"); //node inquirer module 
+
+//Sets up connection to MySQL table
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -9,23 +13,25 @@ var connection = mysql.createConnection({
 	database: "Bamazon"
 });
 
+//Initiates Connection
 connection.connect(function(err){
 	if (err) throw err;
 });
 
-//Console logs all the products in the table
+//Queries for all items in the table
 connection.query("SELECT * FROM products", function(err, res) {
     if(err) throw err;
 
-    //Creates an array that stores the quantities per item in the database, saving us the need for a second SELECT query
+    //An array for storing the quantities of each item in the database, saving us the need for a second SELECT query
     var items = [];
 
+    //Console logs each item in the table and adds its price and quantity to the array
     for (var key in res) {
       console.log(res[key].item_id + ": " + res[key].product_name + ", $" + res[key].price);  
-      items[parseInt(res[key].item_id)] = {item_price: res[key].price, item_quantity: res[key].stock_quantity};
-      
+      items[parseInt(res[key].item_id)] = {item_price: res[key].price, item_quantity: res[key].stock_quantity}; 
     }
 
+    //Asks the user which item they want and how many they want to buy
 	inquirer.prompt([{
 		type: "input",
 		message: "Enter the ID of the item you would like to purchase",
@@ -37,20 +43,25 @@ connection.query("SELECT * FROM products", function(err, res) {
 	}])
 	.then(function(purchase){
 		var item = items[parseInt(purchase.item)];
+
+		//If the quantity they want to buy is more than in the store, tells them they can't do that
 		if(purchase.quantity > item.item_quantity) {
 			console.log("Out of stock lah");
 		}
 		else {
+			//Calculates the total cost to the customer
 			var total = item.item_price * purchase.quantity;
 			console.log("Thank you! That will be $" + total);
+
+			//Updates the table with the new quantity for that item
 			var newQuantity = item.item_quantity - purchase.quantity;
 			var myQuery = "UPDATE products SET stock_quantity=" + newQuantity + " WHERE item_ID=" + parseInt(purchase.item);
-			connection.query(myQuery, function(err, res){
-				console.log("Table updated");
+			connection.query(myQuery, function(err){
+				if (err) throw err;
 			});
 		}
 
-		process.exit();
+		connection.end();
 	});
 
 });
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,13 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var consoleTable = require("console.table");
+//SETUP
+// ===========================================================
 
+var mysql = require("mysql"); //node MySQL module
+var inquirer = require("inquirer"); //node inquirer module 
+var consoleTable = require("console.table"); //node console.table module for console logging prettily
+
+var lowAmount = 500; //the threshold for items to appear on the low inventory report
+
+//Sets up connection to MySQL table
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -10,23 +16,32 @@ var connection = mysql.createConnection({
 	database: "Bamazon"
 });
 
+//Initiates Connection
 connection.connect(function(err){
 	if (err) throw err;
 });
 
+// HUMAN INTERACTION CODE
+// ===========================================================
+
+//Asks what the manager would like to do
 inquirer.prompt({
 
 	type: "list",
 	message: "What would you like to do?",
-	choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+	choices: ["View Products for Sale", "View Low Inventory", "Add Quantity to Inventory", "Add New Product"],
 	name: "choice"
 
 }).then(function(response){
 
+	//Executes the sections of the switch statement based on their response
 	switch(response.choice) {
 
+		// ===========================================================
+		
 		case "View Products for Sale":
 
+			//Console logs the entire products table
 			connection.query("SELECT * FROM products", function(err, res) {
 				if (err) throw err;
 				console.table(res);
@@ -35,9 +50,12 @@ inquirer.prompt({
 
 			break;
 
+		// ===========================================================
+
 		case "View Low Inventory":
 
-			connection.query("SELECT * FROM products WHERE stock_quantity < 500", function(err, res) {
+			//Console logs items with low quantities in inventory
+			connection.query("SELECT * FROM products WHERE stock_quantity < " + lowAmount, function(err, res) {
 				if (err) throw err;
 				console.table(res);
 				connection.end();
@@ -45,8 +63,11 @@ inquirer.prompt({
 
 			break;
 
-		case "Add to Inventory":
+		// ===========================================================
 
+		case "Add Quantity to Inventory": //Update the quantity of an item on the table
+
+			//Queries and builds an array of all items and their quantities, and then console logs them
 			connection.query("SELECT * FROM products", function(err, res) {
     			if(err) throw err;	
 
@@ -59,6 +80,7 @@ inquirer.prompt({
 
 			    console.table(items);
 			
+				//Prompts for which item to add inventory for, and how many
 				inquirer.prompt([{
 
 					type: "input",
@@ -73,11 +95,13 @@ inquirer.prompt({
 
 				}])
 				.then(function(response){
+					//Creates the query using the user's responses
 					var itemNum = parseInt(response.itemNum);
 					var shipmentAmount = parseInt(response.shipmentAmount);
 					var newQuantity = items[itemNum].item_quantity + shipmentAmount;
 					var myQuery = "UPDATE products SET stock_quantity=" + newQuantity + " WHERE item_ID=" + itemNum;
 					
+					//Runs the query to update the table
 					connection.query(myQuery, function(err) {
 						if(err) throw err;
 						console.log("Quantity updated.");
@@ -89,41 +113,45 @@ inquirer.prompt({
 
 			break;
 
-		case "Add New Product":
+		// ===========================================================
 
-			connection.query("DESCRIBE products", function(err, res) {
+		case "Add New Product": //Add a new product row to the table
 
-				inquirer.prompt([{
-					type: "input",
-					message: "Enter product name.",
-					name: "product_name"
-				}, {
-					type: "input",
-					message: "Enter department name of product.",
-					name: "department_name"
-				}, {
-					type: "input",
-					message: "Enter product price.",
-					name: "price"
-				}, {
-					type: "input",
-					message: "Enter starting stock quantity.",
-					name: "stock_quantity"
-				}])
-				.then(function(response){
-					var values = "(NULL, '" + response.product_name + "', '" + response.department_name 
-								+ "', " + response.price + ", " + response.stock_quantity + ")"
-					connection.query("INSERT INTO products VALUES " + values, function(err) {
-						if(err) throw err;
-					});
-					console.log("Item added to registry.");
-					connection.end();
+			//Prompts for product name, department, price, and quantity
+			inquirer.prompt([{
+				type: "input",
+				message: "Enter product name.",
+				name: "product_name"
+			}, {
+				type: "input",
+				message: "Enter department name of product.",
+				name: "department_name"
+			}, {
+				type: "input",
+				message: "Enter product price.",
+				name: "price"
+			}, {
+				type: "input",
+				message: "Enter starting stock quantity.",
+				name: "stock_quantity"
+			}])
+			.then(function(response){
+				//Builds the values part of the query
+				var values = "(NULL, '" + response.product_name + "', '" + response.department_name 
+							+ "', " + response.price + ", " + response.stock_quantity + ")";
+				//Runs the query to add the new row
+				connection.query("INSERT INTO products VALUES " + values, function(err) {
+					if(err) throw err;
 				});
-			});		
+				console.log("Item added to registry.");
+				connection.end();
+			});
 
 			break;
 
-		default:
+		// ===========================================================
+
+		default: //Is it worth even having a default when there's no way to access it?
 
 	}
 	
